a37ccd1b0efe263483a129c8b31c546cff570278
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2014-09-25 08:40:54 +0400

    annual report

---
+++ app/controllers/annual_report_controller.rb
@@ -0,0 +1,14 @@
+class AnnualReportController < ApplicationController
+  def index
+    yearp = params[:year]
+    annual_report_data = OverallReportView.where{year == yearp}
+    @report = {}
+
+    for record in annual_report_data
+      r = (@report[record.location_name] ||= {location_name: record.location_name, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0, :q=>0, :s=>0})
+      r[record.month.to_s] = record.scores
+      r["s"] = record.scores
+      r["q"] = record.scores
+    end
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    overall_report_data = OverallReportView.all
-    report = {}
-
-    for record in overall_report_data
-      r = (report[record.location_name] ||= {location_name: record.location_name, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0, :q=>0, :s=>0})
-      r[record.month.to_s] = record.scores
-      r["s"] = record.scores
-      r["q"] = record.scores
-    end
-
-    @overall_report = report
+  	@year = DateTime.now.year
   end
 end
