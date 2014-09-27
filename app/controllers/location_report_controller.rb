3a56d2cf50dd1900443dbbde6646fc8efb17e588
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2014-09-27 09:14:40 +0400

    simple location report

--- app/controllers/location_report_controller.rb
+++ app/controllers/location_report_controller.rb
@@ -1,15 +1,16 @@
 class LocationReportController < ApplicationController
   def index
-  	#locationp = params[:location]
-    #yearp = params[:year]
-    #location_report_data = Records.where{year == yearp}
-    #@report = {}
+  	locationp = params[:location]
+    yearp = params[:year]
+    location_report_data = Record.joins{report}.joins{report.location}.where{report.year==yearp && report.location.url==locationp}.all
 
-    #for record in annual_report_data
-    #  r = (@report[record.location_name] ||= {location_name: record.location_name, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0, :q=>0, :s=>0})
-    #  r[record.month.to_s] = record.scores
-    #  r["s"] = record.scores
-    #  r["q"] = record.scores
-    #end
+    @report = {}
+
+    for record in location_report_data
+      r = (@report[record.name] ||= {name: record.name, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0, :q=>0, :s=>0})
+      #r[record.month.to_s] = record.scores
+      #r["s"] = record.scores
+      #r["q"] = record.scores
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+ 
   get 'annual/:year' => 'annual_report#index', as: :annual_report
   get ':location/:year' => 'location_report#index', as: :location_report
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
